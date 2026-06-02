9bb9416384255b02d32f0dc8fce5a18e250ea45d
Joao Santos <dev.joaosantos@outlook.com>
2026-06-02 10:10:16 -0300

    feat(core): implement error handling for delete answer comment use case

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,29 +2,30 @@ import { makeAnswerComment } from "@test/factories/make-answer-comment";
 import { InMemoryAnswerCommentsRepository } from "@test/repositories/in-memory-answer-comments-repository";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { DeleteAnswerCommentUseCase } from "./delete-answer-comment";
+import { NotAllowedError } from "./error/not-allowed-error";
 
 describe("Delete answer comment use case", () => {
-  let answerComment: InMemoryAnswerCommentsRepository;
+  let answerCommentRepo: InMemoryAnswerCommentsRepository;
   let sut: DeleteAnswerCommentUseCase;
 
   beforeEach(() => {
-    answerComment = new InMemoryAnswerCommentsRepository();
-    sut = new DeleteAnswerCommentUseCase(answerComment);
+    answerCommentRepo = new InMemoryAnswerCommentsRepository();
+    sut = new DeleteAnswerCommentUseCase(answerCommentRepo);
   });
 
   test("it should be able to delete a answer", async () => {
     const { newAnswerComment } = makeAnswerComment();
 
-    answerComment.create(newAnswerComment);
+    answerCommentRepo.create(newAnswerComment);
 
-    expect(answerComment.answerComments).toHaveLength(1);
+    expect(answerCommentRepo.answerComments).toHaveLength(1);
 
     await sut.execute({
       answerCommentId: newAnswerComment.id.toString(),
       authorId: newAnswerComment.authorId.toString(),
     });
 
-    expect(answerComment.answerComments).toHaveLength(0);
+    expect(answerCommentRepo.answerComments).toHaveLength(0);
   });
 
   test("it should not be able to delete a answer if has a different authorId", async () => {
@@ -32,15 +33,16 @@ describe("Delete answer comment use case", () => {
       authorId: new UniqueEntityID("author-1"),
     });
 
-    answerComment.create(newAnswerComment);
+    await answerCommentRepo.create(newAnswerComment);
 
-    expect(answerComment.answerComments).toHaveLength(1);
+    const result = await sut.execute({
+      answerCommentId: newAnswerComment.id.toString(),
+      authorId: "author-2",
+    });
 
-    await expect(() =>
-      sut.execute({
-        answerCommentId: newAnswerComment.id.toString(),
-        authorId: "author-2",
-      }),
-    ).rejects.toBeInstanceOf(Error);
+    expect(answerCommentRepo.answerComments).toHaveLength(1);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,12 +1,17 @@
 import { type Either, left, right } from "@/core/either";
 import type { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
+import { NotAllowedError } from "./error/not-allowed-error";
+import { ResourceNotFoundError } from "./error/resource-not-found-error";
 
 interface DeleteAnswerCommentUseCaseParams {
   answerCommentId: string;
   authorId: string;
 }
 
-type DeleteAnswerCommentUseCaseReturn = Either<"string", {}>;
+type DeleteAnswerCommentUseCaseReturn = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>;
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentRepository: AnswerCommentsRepository) {}
@@ -19,11 +24,11 @@ export class DeleteAnswerCommentUseCase {
       await this.answerCommentRepository.getById(answerCommentId);
 
     if (!answerComment) {
-      return left("Entity not found.");
+      return left(new ResourceNotFoundError());
     }
 
     if (authorId !== answerComment.authorId.toString()) {
-      return left("Not allowed.");
+      return left(new NotAllowedError());
     }
 
     await this.answerCommentRepository.delete(answerComment);
